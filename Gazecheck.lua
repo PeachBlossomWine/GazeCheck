f221a2efd25ed52c95ff8016aed5295f4cf23823
PeachBlossomWine <52470877+PeachBlossomWine@users.noreply.github.com>
2024-02-16 10:36:33 -0500

    Update Gazecheck.lua
    Ambu newer (lamia)

--- Gazecheck.lua
+++ Gazecheck.lua
@@ -11,28 +11,34 @@ packets = require('packets')
 defaults = {
 	ap = false,
 	auto_gaze = true,
-	auto_perm_gaze = true
+	auto_perm_gaze = true,
+	weak_control = false,
 }
+
+__weakened = false
 
 gaol_zones = S{279,298}
 settings = config.load(defaults)
 
 gaze_attacks = {[284]="Cold Stare",[292]="Blank Gaze",[370]="Baleful Gaze",[386]="Awful Eye",[411]="Baleful Gaze",[438]="Hex Eye",[439]="Petro Gaze",
 [502]="Mortal Ray",[550]="Hypnosis",[551]="Mind Break",[577]="Jettatura",[586]="Blank Gaze",[589]="Mortal Ray",[648]="Petro Eyes",[653]="Chaotic Eye",
-[785]="Light of Penance",[1111]="Numbing Glare",[1113]="Tormentful Glare",[1115]="Torpid Glare",[1138]="Hypnosis",[1139]="Mind Break",[1174]="Petro Eyes",
+[785]="Light of Penance",[1111]="Numbing Glare",[1113]="Tormentful Glare",[1138]="Hypnosis",[1139]="Mind Break",[1174]="Petro Eyes",
 [1184]="Petro Eyes",[1322]="Gerjis' Grip",[1359]="Chthonian Ray",[1360]="Apocalyptic Ray",[1563]="Cold Stare",[1603]="Baleful Gaze",[1680]="Predatory Glare",
 [1694]="Vile Belch",[1695]="Hypnic Lamp",[1713]="Yawn",[1716]="Frigid Shuffle",[1759]="Hypnotic Sway",[1762]="Belly Dance",[1862]="Awful Eye",[1883]="Mortal Ray",
 [1950]="Belly Dance",[1978]="Abominable Belch",[2111]="Eternal Damnation",[2155]="Torpefying Charge",[2209]="Blink of Peril",[2424]="Terror Eye",[2465]="Bill Toss",
 [2466]="Washtub",[2570]="Afflicting Gaze",[2534]="Minax Glare",[2602]="Mortal Blast",[2610]="Vacant Gaze",[2768]="Deathly Glare",[2814]="Yawn",[2817]="Frigid Shuffle",
 [2828]="Jettatura",[3031]="Sylvan Slumber",[3032]="Crushing Gaze",[3358]="Blank Gaze",[3760]="Beguiling Gaze",[3898]="Chaotic Eye",[3916]="Jettatura",
-
+--[1115]="Torpid Glare",
 [4096]="Pain Sync",
 [4192]="Fatal Allure",
-[3968]="Petrifaction",[3975]="Petrifying Dance",[3977]="Venomous Dance",[3978]="Raqs Baladi",
+[277]="Dread Spikes",
+[3968]="Petrifaction",
+--[3975]="Petrifying Dance",[3977]="Venomous Dance",[3978]="Raqs Baladi",
+[3975]="Petrifactive Dance",[3977]="Poisonous Dance",[3978]="Luxurious Dance",
 
 }
 
-perm_gaze_attacks = {[2156]="Grim Glower",[2392]="Oppressive Glare",[2776]="Shah Mat",[4121]="Repulsor",}
+perm_gaze_attacks = {[2156]="Grim Glower",[2392]="Oppressive Glare",[2776]="Shah Mat",[4121]="Repulsor",} --[3026]="Incinerating Lahar",
 --,[3980]="Lateral Slice"
 --,[3981]="Vertical Slice"
 perm_gaze_control = {["Peiste"]={skills=T{2156, 2392},delay=3,ender=T{4}},["Caturae"]={skills=T{2776},delay=6,ender=T{4,6}},["Quadav"]={skills=T{4121},delay=2,ender=T{4}},["Goblin"]={skills=T{4121},delay=2,ender=T{4}},["Orc"]={skills=T{4121},delay=2,ender=T{4}},}
@@ -83,7 +89,7 @@ function check_facing(packet)
     local party = windower.ffxi.get_party()
     local actor = windower.ffxi.get_mob_by_id(packet['Actor'])
     local player = windower.ffxi.get_mob_by_target('me')
-    local dir = {actor=(V{player.x, player.y} - V{actor.x, actor.y}),player=(V{actor.x, actor.y} - V{player.x, player.y})}
+    local dir = actor and {actor=(V{player.x, player.y} - V{actor.x, actor.y}),player=(V{actor.x, actor.y} - V{player.x, player.y})}
     local heading = {actor=(V{}.from_radian(actor.facing)),player=(V{}.from_radian(player.facing))}
     local angle = {actor=(V{}.angle(dir.actor, heading.actor):degree():abs()),player=(V{}.angle(dir.player, heading.player):degree():abs())}
     for i,v in pairs(packet) do
@@ -113,7 +119,9 @@ end
 function check_target_action(packet)
     for i,v in pairs(packet) do
         if string.match(i, 'Target %d+ Action %d+ Param') then
+		--windower.add_to_chat(7,"Mob ability/spell ID = "..tostring(v))
             if settings.auto_gaze and gaze_attacks[v] then
+				windower.add_to_chat(7,"Mob ability/spell ID = "..tostring(v))
                 return true
             elseif settings.auto_perm_gaze and perm_gaze_attacks[v] then
                 for mob,tbl in pairs(perm_gaze_control) do
@@ -159,9 +167,19 @@ windower.register_event('incoming chunk', function(id, data, modified, injected,
             if packet['Category'] == 7 and not pet_check(packet['Actor']) and (check_target_id(packet) or check_facing(packet)) and check_target_action(packet) then
                 gaze = true
                 trigered_actor = packet['Actor']
-                if NotDead() then
+                if NotDead() then -- Turn around here
                     windower.ffxi.turn((getAngle(packet['Actor'])+180):radian()+math.pi)
+					-- if haveBuff('Weakness') then
+						-- __weakened = true
+					-- --	move_from_to()
+					-- end
                 end
+			-- elseif packet['Category'] == 4 and not pet_check(packet['Actor']) and check_facing(packet) and check_target_action(packet) then
+                -- gaze = true
+                -- trigered_actor = packet['Actor']
+                -- if NotDead() then -- Turn around here
+                    -- windower.ffxi.turn((getAngle(packet['Actor'])+180):radian()+math.pi)
+                -- end
             elseif packet['Category'] == 11 and packet['Actor'] == trigered_actor and gaze then
                 if settings.auto_gaze and gaze_attacks[packet['Param']] and not perm_gaze then
                     gaze = false
@@ -176,6 +194,51 @@ windower.register_event('incoming chunk', function(id, data, modified, injected,
         end
     end
 end)
+
+function move_from_to()
+	local min_distance = 13
+	local t = windower.ffxi.get_mob_by_target('t') or nil
+	local p = windower.ffxi.get_player()
+	--if (S{'RUN','PLD'}:contains(p.main_job) or not t or not p.in_combat) then return end
+	
+	if __weakened and not (S{'RUN','PLD'}:contains(p.main_job)) then
+		if p.target_locked then
+			windower.send_command("wait 1; input /lockon")
+		end
+		coroutine.sleep(2.5)
+		windower.ffxi.run(true)
+		while math.sqrt(windower.ffxi.get_mob_by_index(t.index).distance) <= min_distance do
+			p_2 = windower.ffxi.get_mob_by_index(p.index)
+			coroutine.sleep(0.15)
+		end
+		windower.ffxi.run(false)
+	elseif not (S{'RUN','PLD'}:contains(p.main_job)) then
+		if not p.target_locked then
+			windower.send_command("wait 1; input /lockon")
+		end
+		coroutine.sleep(2.5)
+		windower.ffxi.run(true)
+		while math.sqrt(windower.ffxi.get_mob_by_index(t.index).distance) >= 6 do
+			p_2 = windower.ffxi.get_mob_by_index(p.index)
+			coroutine.sleep(0.15)
+		end
+		windower.ffxi.run(false)
+	end
+end
+
+function haveBuff(...)
+	local args = S{...}:map(string.lower)
+	local player = windower.ffxi.get_player()
+	if (player ~= nil) and (player.buffs ~= nil) then
+		for _,bid in pairs(player.buffs) do
+			local buff = res.buffs[bid]
+			if args:contains(buff.en:lower()) then
+				return true
+			end
+		end
+	end
+	return false
+end
 
 function getAngle(index)
     local P = windower.ffxi.get_mob_by_target('me') --get player
@@ -195,6 +258,17 @@ function isMonster()
         return false
 	end
 end
+
+function handle_lose_buff(buff_id)
+	if buff_id == 1 then
+		__weakened = false
+		if gaze and perm_gaze then
+			--move_from_to()
+		end
+		gaze = false
+		perm_gaze = false
+	end
+end	
 
 windower.register_event('prerender', function()
     local player = windower.ffxi.get_player()
@@ -220,6 +294,8 @@ function haveBuff(...)
 	return false
 end
 
+windower.register_event('lose buff', handle_lose_buff)
+
 windower.register_event('addon command', function(input, ...)
 
 	local cmd
@@ -242,6 +318,18 @@ windower.register_event('addon command', function(input, ...)
 		elseif cmd2 == 'off' then
 			settings.auto_gaze = false
 		end
+	elseif cmd == 'weak' then
+		if cmd2 == 'on' then
+			settings.weak_control = true
+		elseif cmd2 == 'off' then
+			settings.weak_control = false
+		end
+	elseif cmd == 'testw' then
+			__weakened = true
+			move_from_to()
+	elseif cmd == 'testu' then
+			__weakened = false
+			move_from_to()
 	end
 	
     if cmd then
